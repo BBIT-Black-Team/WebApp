ca70c22cefcf880370a9b751ce3780f7297ef359
Riken <rikenshah.02@gmail.com>
2020-07-14 23:20:38 +0530

    Added SB Admin 2

---
+++ WebApp/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(label='Email', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+
--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from WebApp.forms import LoginForm
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(email, password)
+    return render(request, 'home/login.html')
+
