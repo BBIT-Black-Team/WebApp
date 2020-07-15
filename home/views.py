a73247e643ea750fdb6d1ec230d499c9dcf83507
Riken <rikenshah.02@gmail.com>
2020-07-15 15:32:35 +0530

    Added Login Backend

--- home/views.py
+++ home/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from WebApp.forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        print(email, password)
-    return render(request, 'home/login.html')
+        form = AuthenticationForm(request=request, data=request.POST)
 
+        print("Here", request.POST, form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            print(username, password)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/login')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request,
+                  template_name="home/login.html",
+                  context={"form": form})
